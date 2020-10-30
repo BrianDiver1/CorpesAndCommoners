89b5067a891fdd20e8330aadb67b4ca162157d0b
BrianDiver1 <71856460+BrianDiver1@users.noreply.github.com>
2020-10-29 23:07:30 -0400

    Zombie Behavior Coding

--- Game Jam/Assets/Scripts/ZombieBehavior.cs
+++ Game Jam/Assets/Scripts/ZombieBehavior.cs
@@ -1,32 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class ZombieBehavior : MonoBehaviour
 {
     public float speed;
-    public float rotateSpeed;
     public int health;
-    public Vector2 position;
-    public Vector2 target;
-    public Vector2 mousePosition;
-    Event currentEvent = Event.current;
+    Vector3 target;
+    public CircleCollider2D detectionRadius;
+    public Animation attack;
     // Start is called before the first frame update
     void Start()
     {
-        position = gameObject.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 screenPosition = Mouse.current.position.ReadValue();
-        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        target = worldPosition;
-        //this.transform.LookAt(target);
-        transform.position = Vector2.MoveTowards(gameObject.transform.position, target, speed * Time.deltaTime);
-        transform.up = -(target * rotateSpeed * Time.deltaTime);
-        //transform.rotation = Quaternion.LookRotation(Vector2.up, Vector2.MoveTowards(gameObject.transform.position, target, rotateSpeed * Time.deltaTime));
+        FollowMouse();
+    }
+    void FollowMouse()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
+        transform.position = Vector2.MoveTowards(transform.position, transform.position - transform.up, speed * Time.deltaTime);
+        transform.up = -(target - transform.position);
+    }
+    void Attack(GameObject victim)
+    {
+        while(Vector2.Distance(this.transform.position,victim.transform.position) > 10)
+        {
+            target = victim.transform.position;
+            if (Vector2.Distance(this.transform.position, victim.transform.position) > 50)
+                return;
+        }
+        attack.Play();
     }
 }
+
