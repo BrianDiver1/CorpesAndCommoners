1517817c801203841e4d246d8aafe4bca7ffdd75
BrianDiver1 <71856460+BrianDiver1@users.noreply.github.com>
2020-10-30 01:50:16 -0400

    Combat technically works

--- Game Jam/Assets/Scripts/PeasantBehavior.cs
+++ Game Jam/Assets/Scripts/PeasantBehavior.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PeasantBehavior : MonoBehaviour
 {
     public float speed;
     public int health;
-    Vector3 target;
     public CircleCollider2D humanCollision;
     public GameObject badGuyParent;
     public float deathDelay;
@@ -15,18 +15,22 @@ public class PeasantBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        zombieHitBox = badGuyParent.GetComponentInChildren<Collider2D>();
         if (humanCollision.IsTouching(zombieHitBox))
             this.health -= 1;
         if(health <= 0)
         {
-            Destroy(this, deathDelay);
-            Instantiate(zombiePrefab);
+            Destroy(gameObject, deathDelay);
+            Instantiate(zombiePrefab, badGuyParent.transform);
         }
     }
 }
+
+
+
